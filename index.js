813e884ce2564c718a76a59a828f81eb08f0a5f2
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-04 22:16:30 +0200

    modulify

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-var MAX_SAFE_INTEGER = 9007199254740991;
+var MAX_SAFE_INTEGER = require('max-safe-integer');
 
 module.exports = Number.isSafeInteger || function (val) {
 	return typeof val === 'number' && val === val && val !== Infinity && val !== -Infinity && parseInt(val, 10) === val && Math.abs(val) <= MAX_SAFE_INTEGER;
